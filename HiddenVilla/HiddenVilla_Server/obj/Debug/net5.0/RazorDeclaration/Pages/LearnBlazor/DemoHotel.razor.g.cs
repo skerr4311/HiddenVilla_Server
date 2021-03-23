eabfad6c8493e65929939874347ead91dfbcd821
Steve <sker078@aucklanduni.ac.nz>
2021-03-24 09:14:12 +1300

    Updated child component

--- HiddenVilla/HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Pages/LearnBlazor/DemoHotel.razor.g.cs
+++ HiddenVilla/HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Pages/LearnBlazor/DemoHotel.razor.g.cs
@@ -13,84 +13,84 @@ namespace HiddenVilla_Server.Pages.LearnBlazor
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 1 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 2 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 3 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 4 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 5 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 6 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 7 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 8 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 9 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 10 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 11 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server.Pages.LearnBlazor.LearnBlazorComp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\Pages\LearnBlazor\DemoHotel.razor"
+#line 2 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\Pages\LearnBlazor\DemoHotel.razor"
 using Model;
 
 #line default
@@ -105,7 +105,7 @@ using Model;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 33 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\Pages\LearnBlazor\DemoHotel.razor"
+#line 33 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\Pages\LearnBlazor\DemoHotel.razor"
       
     public int SelectedRooms { get; set; } = 0;
     public string SelectedAminity { get; set; } = "";
--- HiddenVilla/HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ HiddenVilla/HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,77 +13,77 @@ namespace HiddenVilla_Server.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 1 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 2 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 3 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 4 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 5 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 6 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 7 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 8 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 9 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 10 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\skerr\source\repos\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 11 "C:\Users\skerr\source\Unity_Projects\HiddenVilla_Server\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server.Pages.LearnBlazor.LearnBlazorComp;
 
 #line default
